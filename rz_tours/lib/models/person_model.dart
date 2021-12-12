98534acdb65f74693c62fc6c77268cb48ae1cad1
kareem337 <74376900+kareem337@users.noreply.github.com>
2021-12-12 19:42:42 +0200

    classes

---
+++ rz_tours/lib/models/person_model.dart
@@ -0,0 +1,23 @@
+class Person
+{
+  late String _firstName;
+  late String _lastName;
+  late String _email;
+  late String _password;
+  late int _number;
+  late String _gender;
+  late int _id;
+  late String _image;
+
+login()
+{
+
+}
+
+chat()
+{
+  
+}
+
+
+}
---
+++ rz_tours/lib/models/user_model.dart
@@ -0,0 +1,29 @@
+import 'package:rz_tours/models/person_model.dart';
+
+class User extends Person
+{
+  register()
+  {
+
+  }
+  
+  editProfile()
+  {
+
+  }
+
+  addToCart()
+  {
+
+  }
+
+  deleteFromCart()
+  {
+
+  }
+
+  purchase()
+  {
+
+  }
+}
