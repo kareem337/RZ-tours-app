771f9baa3d35b4d58866fa67ba26a0d98ddc0746
kareem337 <74376900+kareem337@users.noreply.github.com>
2021-12-12 20:05:07 +0200

    routes

--- rz_tours/lib/main.dart
+++ rz_tours/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:rz_tours/screens/home.dart';
 import 'package:rz_tours/screens/edit_profile.dart';
 
+import 'screens/sign_in.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -12,6 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '\home',
+      routes: {
+        "\home": (context) => Home(),
+        "\login": (context) => SignIn(),
+      
+      },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
