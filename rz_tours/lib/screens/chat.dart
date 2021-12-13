ad08b236ac5fdba121d52ff999f57323a0ebd5e4
Loay Yehia <75745952+loayyehia9@users.noreply.github.com>
2021-12-13 14:43:38 +0200

    .

--- rz_tours/lib/screens/chat.dart
+++ rz_tours/lib/screens/chat.dart
@@ -16,63 +16,117 @@ class _ChatState extends State<Chat> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Row(
-          children: [
-            // ignore: prefer_const_constructors
-            CircleAvatar(
-              radius: 20,
-              // ignore: prefer_const_constructors
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  width: 2,
+                ),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "<https://randomuser.me/api/portraits/men/5.jpg>"),
+                  maxRadius: 20,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Name",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.settings,
+                  color: Colors.black54,
+                ),
+              ],
             ),
-            Column(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [Text("Name of The User"), Text("Online")],
-            )
-          ],
+          ),
         ),
       ),
-      bottomNavigationBar: Buttom(),
       body: Container(
-        // ignore: prefer_const_constructors
-        padding: EdgeInsets.all(20),
-        // ignore: prefer_const_constructors
-        margin: EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            Container(
-              color: Colors.blueGrey,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              child: Text('$s'),
-              color: Colors.greenAccent,
-              width: 100,
-              height: 100,
-            ),
-            TextField(
-              onChanged: (val) {
-                s = val;
-              },
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), hintText: ' Send a message'),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                // ignore: prefer_const_constructors
-                Text("Hello"),
-                // ignore: prefer_const_constructors
-                IconButton(
-                    onPressed: () {
-                      setState(() {});
-                    },
-                    icon: Icon(Icons.send))
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+            height: 60,
+            width: double.infinity,
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Write message...",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                    size: 18,
+                  ),
+                  backgroundColor: Colors.blue,
+                  elevation: 0,
+                ),
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
