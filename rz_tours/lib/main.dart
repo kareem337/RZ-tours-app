662800af9857ae56b61e4a3e741d333b0e8b28c0
Loay Yehia <75745952+loayyehia9@users.noreply.github.com>
2021-12-19 23:16:02 +0200

    update main.dart

--- rz_tours/lib/main.dart
+++ rz_tours/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:rz_tours/screens/home.dart';
-import 'package:rz_tours/screens/home_screen.dart';
 import 'package:rz_tours/screens/sign_in.dart';
 import 'package:rz_tours/utils/constants.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -18,19 +15,17 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: Size(375, 812),
       builder: () => MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Constants.primaryColor,
-        primarySwatch: Colors.grey,
-        scaffoldBackgroundColor: Color.fromRGBO(247, 249, 255, 1),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.poppinsTextTheme(),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Constants.primaryColor,
+          primarySwatch: Colors.grey,
+          scaffoldBackgroundColor: Color.fromRGBO(247, 249, 255, 1),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        home: SignIn(),
       ),
-      home: SignIn(),
-    ),
-
     );
   }
 }
-
