fe14d0541e3c7f5cc7483020cdaf108a0dbd8401
Loay Yehia <75745952+loayyehia9@users.noreply.github.com>
2022-01-20 21:14:25 +0200

    update product model

--- rz_tours/lib/main.dart
+++ rz_tours/lib/main.dart
@@ -5,6 +5,10 @@ import 'package:rz_tours/screens/sign_in.dart';
 import 'package:rz_tours/utils/constants.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rz_tours/utils/error.dart';
+import 'package:rz_tours/utils/loading.dart';
+import 'package:rz_tours/utils/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,26 +18,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider (
-      
-      create: (context) =>  Cart(),
-      child:
-      ScreenUtilInit(
-      designSize: Size(375, 812),
-      builder: () => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: Constants.primaryColor,
-          primarySwatch: Colors.grey,
-          scaffoldBackgroundColor: Color.fromRGBO(247, 249, 255, 1),
-          textTheme: GoogleFonts.poppinsTextTheme(),
-        ),
-        home: SignIn(),
-        ),
-    )
-    );
-    
-
+    return ChangeNotifierProvider(
+        create: (context) => Cart(),
+        child: ScreenUtilInit(
+          designSize: Size(375, 812),
+          builder: () => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primaryColor: Constants.primaryColor,
+              primarySwatch: Colors.grey,
+              scaffoldBackgroundColor: Color.fromRGBO(247, 249, 255, 1),
+              textTheme: GoogleFonts.poppinsTextTheme(),
+            ),
+            home: SignIn(),
+          ),
+        ));
   }
 }
